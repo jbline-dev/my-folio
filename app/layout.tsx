36aba056e6c2889b25841b64fd6413c4d07e15a4
John Robert Gaufo <johnrobertgaufo1@gmail.com>
2026-09-24 10:43:35 +0800

    Replaced favicon with my brand logo

--- app/layout.tsx
+++ app/layout.tsx
@@ -21,20 +21,7 @@ export const metadata: Metadata = {
     'A software engineer’s personal digital workspace. I build digital products, interfaces and systems.',
   generator: 'v0.app',
   icons: {
-    icon: [
-      {
-        url: '/icon-light-32x32.png',
-        media: '(prefers-color-scheme: light)',
-      },
-      {
-        url: '/icon-dark-32x32.png',
-        media: '(prefers-color-scheme: dark)',
-      },
-      {
-        url: '/icon.svg',
-        type: 'image/svg+xml',
-      },
-    ],
+    icon: '/icon.png',
     apple: '/apple-icon.png',
   },
 }
